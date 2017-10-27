96e79af284e1177d5be655bccf28e351a0e3dd9a
rhatyang <fanzhiyang2001@gmail.com>
2017-10-27 09:34:25 +0800

    删除没用的js

--- assets/script/main.js
+++
@@ -1,7 +0,0 @@
-(function(){
-    var oTop = document.getElementsByClassName("top")[0];
-    if (document.body.offsetWidth < 800 ) {
-        oTop.innerHTML = "建议使用电脑浏览，以获得更佳体验";
-    }
-    document.getElementsByTagName("svg")[0].style.top = oTop.offsetHeight +"px";
-})();
