647c4dd0aa73a6b9ad4055effe05a4bfabea39bf
rhatyang <fanzhiyang2001@gmail.com>
2018-01-30 09:13:24 +0800

    更新

--- utils/clean.js
+++ utils/clean.js
@@ -2,13 +2,13 @@ const rmrf = require('rimraf'),
   path = require('path'),
   config = require('../lib/config');
 
-rmrf(config.public_dir,(err) => {
+rmrf(config.public_dir, err => {
   if (!err) {
     console.log('删除生成文件目录成功')
   }
 });
 
-rmrf(config.postsDb,(err) => {
+rmrf(config.postsDb, err => {
   if (!err) {
     console.log('删除文章数据库成功')
   }
